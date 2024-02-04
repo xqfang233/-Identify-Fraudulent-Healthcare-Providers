f56d8d188f994ea1a8bc16b3dd9179a9efcc0a93
unknown <xqfang12@gmail.com>
2024-02-04 14:10:48 -0500

    update main.py, openAPI.yaml and app.yaml for deployment on GCP

--- main.py
+++ main.py
@@ -2,17 +2,16 @@ from flask import Flask, render_template, request
 import pickle
 
 app = Flask(__name__)
-
-
-model = predict_fraud()
+model = pickle.load(open('xgboost_model.pkl', 'rb'))
 @app.route('/',methods=['GET'])
 def Home():
     return render_template('index.html')
 
 
 @app.route("/predict", methods=['POST'])
-def recieve_data():
+def predict():
     if request.method == 'POST':
+
         In_ClaimID = float(request.form['In_ClaimID'])
         In_BeneID=float(request.form['In_BeneID'])
         In_claim_duration=float(request.form['In_claim_duration'])
@@ -25,20 +24,19 @@ def recieve_data():
         dp = [In_ClaimID, In_BeneID, In_claim_duration, In_DeductibleAmtPaid,
                                   Ot_12Months_PartBCov, In_InscClaimAmtReimbursed, Ot_ClmDiagnosisNum,
                                   Ot_12Months_PartACov]
-        return dp
-        #
-        # prediction=model.predict([dp])
-        # pred_prob = model.predict_proba([dp])
-        # # pred_class = model.classes_
-        # res=prediction[0]
-    #
-    #     if res==0:
-    #         return render_template('index.html',prediction_text="This is not a fraudulent provider. The probability of being non-fraudulent is {}, the probaility of being fraudulent is {}.".format(pred_prob[:,0],pred_prob[:,1]))
-    #     elif res==1:
-    #         return render_template('index.html',prediction_text="You caught a fraudulent provider!!! The probability of being non-fraudulent is {}, the probaility of being fraudulent is {}.".format(pred_prob[:,0],pred_prob[:,1]))
-    # else:
-    #     return render_template('index.html')
-#
-# if __name__=="__main__":
-#     app.run(debug=True)
-#
+
+        prediction=model.predict([dp])
+        pred_prob = model.predict_proba([dp])
+        # pred_class = model.classes_
+        res=prediction[0]
+
+        if res==0:
+            return render_template('index.html',prediction_text="This is not a fraudulent provider. The probability of being non-fraudulent is {}, the probaility of being fraudulent is {}.".format(pred_prob[:,0],pred_prob[:,1]))
+        elif res==1:
+            return render_template('index.html',prediction_text="You caught a fraudulent provider!!! The probability of being non-fraudulent is {}, the probaility of being fraudulent is {}.".format(pred_prob[:,0],pred_prob[:,1]))
+    else:
+        return render_template('index.html')
+
+if __name__=="__main__":
+    app.run(port=8080, debug=True)
+
